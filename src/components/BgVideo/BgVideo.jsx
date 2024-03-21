5f109ac14078c5e24ef3bdbd29de4838ae5b7976
Xtramemium <ivanteev85@gmail.com>
2024-03-21 15:04:24 +0300

    bgVideo changed

--- src/components/BgVideo/BgVideo.jsx
+++ src/components/BgVideo/BgVideo.jsx
@@ -1,5 +1,7 @@
 import bgVid7 from '../../assets/bgVid7.webm';
 import bgVid7_lowQ from '../../assets/bgVid7_lowQ.webm';
+import bgVid7_lowQ2 from '../../assets/bgVid7_lowQ2.mp4';
+import bgVid7_mod from '../../assets/bgVid7_mod.mp4';
 import { SubHeading } from '../SubHeading/SubHeading';
 import { Header } from '../Header/Header';
 import './BgVideo.css';
@@ -16,7 +18,7 @@ export const BackgroundVideo = () => {
 					autoPlay
 					loop
 					muted
-					src={bgVid7_lowQ}
+					src={bgVid7_mod}
 					className="video"
 				/>
 				<div className="bg-video-overlay">
