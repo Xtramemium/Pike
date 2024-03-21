f4cdb53ae83770dbdc7faba4fc5095c8ef96b5ca
Xtramemium <ivanteev85@gmail.com>
2024-03-21 12:51:27 +0300

    metrics deleted

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,12 @@ import { App } from './App.jsx';
 import { Provider } from 'react-redux';
 import { store } from './store.js';
 import { BrowserRouter } from 'react-router-dom';
-import { SpeedInsights } from "@vercel/speed-insights/next"
 import '../index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<BrowserRouter>
 		<Provider store={store}>
 				<App />
-			<SpeedInsights/>
 		</Provider>
 	</BrowserRouter>,
 );
